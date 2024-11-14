4019e750c4f427df41dd602ef98df64ca9cbcde3
kiczyn <38925384+kiczyn@users.noreply.github.com>
2024-11-14 10:33:15 +0100

    fixed request content

--- openapp.py
+++ openapp.py
@@ -15,12 +15,12 @@ client = OpenAI()
 ai=client.chat.completions.create(
     model="gpt-4o",
     messages=[
-        {"role": "system", "content": "bazując na tresc artykulu, napraw kodowanie znaków w tresci, utwórz odpowiednie tagi html do strukturyzacji tresci artykułu wyróżniając tytuły i akapity. Dodaj odpowiednie klasy i id. Zasugeruj w jakich miejscach artykułu warto umieścić grafiki używając w tym celu tagu <img> z atrybutem src='image_placeholder.jpg'. Zwrócony kod powinien zawierać jedynie zawartość do wtawienia pomiędzy tagami <body> i </body>."},
+        {"role": "system", "content": "bazując na tresc artykulu, napraw kodowanie znaków w tresci, utwórz odpowiednie tagi html do strukturyzacji tresci artykułu wyróżniając tytuły i akapity. Dodaj odpowiednie klasy i id. Zasugeruj w jakich miejscach artykułu warto umieścić grafiki używając w tym celu tagu <img> z atrybutem src='image_placeholder.jpg'. Dodaj atrybut alt do każdego obrazka z dokładnym promptem, który możemy użyć do wygenerowania grafiki. Umieść podpisy pod grafikami używając odpowiedniego tagu HTML Zwrócony kod powinien zawierać jedynie zawartość do wtawienia pomiędzy tagami <body> i </body> bez potrójnych apostrofów."},
         {"role": "user", "content": response.text}
     ]
 )
 
-
+print(ai.choices[0].message.content)
 
 Func =open("artykul.html", "w",encoding="utf-8")
 Func.write(ai.choices[0].message.content)
